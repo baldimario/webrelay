e234eb9993b01f9ce5c1c0eb549bc401f3e45750
Mario Baldi <geek0x90@gmail.com>
2021-04-15 21:59:48 +0200

    Update webrelay.ino

--- webrelay.ino
+++ webrelay.ino
@@ -4,7 +4,7 @@
  *  per Nicola 
  *  
  *  Endpoints:
- *  http://IP:PORT/relayXon dove X è intero compreso tra 0 e 3 inclusi
+ *  http://IP:PORT/relayXon dove X è intero compreso tra 1 e 4 inclusi
  *  http://IP:PORT/setup?X=Y dove X è il parametro da settare e Y è il valore
  *    X = 1 IP HOST
  *    X = 2 IP GATEWAY
